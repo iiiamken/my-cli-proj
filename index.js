01e495d883309ab48d2fb8d6598d41a26d7264d2
iiiamken <kenny.t.dinh93@gmail.com>
2024-06-10 09:54:56 +0200

    added more flags with .option

--- index.js
+++ index.js
@@ -5,15 +5,11 @@ import { program } from "commander"
 program
   .version("1.0.0")
   .description("My Node CLI")
-  .options(
-    {
-      name: "Add your name",
-      last: "Add your last name",
-    }
-    // "-ln, -last-name": "add your last name",
-  )
+  .option("-n, --name <type>", "Add your name")
+  .option("-a, --age <number>", "Add your age")
+  .option("-e, --email <string>", "Add your email")
   .action((options) => {
-    console.log(`Hey, ${program.name + program.last}}!`)
+    console.log(`Hey, ${options.name + options.email + options.age}!`)
   })
 
 program.parse(process.argv)
